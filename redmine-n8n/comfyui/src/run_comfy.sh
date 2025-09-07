4725cbb65a54e76cb86265fa5decb73b60c63ef2
CodeMusic <themusicofthecode@gmail.com>
2025-09-07 10:18:22 -0400

    fix

--- redmine-n8n/comfyui/src/run_comfy.sh
+++ redmine-n8n/comfyui/src/run_comfy.sh
@@ -2,5 +2,5 @@
 cd ~/redmine-n8n/comfyui/src
 source .venv/bin/activate
 export PYTORCH_ENABLE_MPS_FALLBACK=1
-export COMFYUI_CUSTOM_NODES="/Users/christopherhicks/redmine-n8n/comfyui/custom_nodes"
+export COMFYUI_CUSTOM_NODES="/Users/christopherhicks/redmine-n8n/comfyui/src/custom_nodes"
 exec python main.py --listen 0.0.0.0 --port 8008 >> ~/redmine-n8n/comfyui/src/comfyui.log 2>&1
